dcb50e46d38fedce3017edde3a024944377d5972
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-09 22:38:56 +0530

    Permitted Conversions Add

---
+++ WideningConversion.java
@@ -0,0 +1,18 @@
+public class WideningConversion {
+    public static void main(String[] args) {
+        // int x;
+        // float y = x;    // error
+
+        /* 
+            Permitted Conversions
+            byte to short, int, long, float, or double
+            short to int, long, float, or double
+            char to int, long, float, or double
+            int to long, float, or double
+            long to float or double
+            float to double
+
+        */
+    }
+    
+}
