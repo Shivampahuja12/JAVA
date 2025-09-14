c81285fa0174a9b11cd1938b51ef0b3b75e09419
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-15 01:44:07 +0530

    Array Start

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,15 @@
+public class ArrayBasics {
+	public static void main(String[] args){
+        // int arr[5];  // error
+        // int[] brr = new int[5] {1,2,3,4,5} error 
+        int[] arr = new int[]{1,2,3,4,5}; // here arr is a reference variable which is pointing/referencing to an dynamically allocated array of size 5
+        int a[] = new int[5];
+        int b[] = {1,2,3,4,5};
+
+
+		
+
+        // array is not blank its always initialized with 0
+        
+	}
+}
