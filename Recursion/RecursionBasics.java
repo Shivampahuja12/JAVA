1d15e918c8d9451cd4c20db75b859892adca222c
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-19 11:57:32 +0530

    Recursion Started

---
+++ Recursion/RecursionBasics.java
@@ -0,0 +1,12 @@
+public class RecursionBasics {
+	public static void main(String[] args){
+		// write a function to print hello world
+		message();
+		message();
+		message();
+		message();
+	}
+	static void message(){
+		System.out.println("Hello World!");
+	}
+}
