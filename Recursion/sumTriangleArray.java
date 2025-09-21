860c877adef2c79227b4c93635fc1e28da23de95
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-21 18:03:52 +0530

    Sum Triangle Array add

---
+++ Recursion/sumTriangleArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class sumTriangleArray {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        sumTriangle(a);
+    }
+
+    static void sumTriangle(int[] a){
+
+        
+        if (a.length < 1){
+            return;
+        }
+        
+        int[] ano = new int[a.length-1];
+        
+        for (int i=0; i<a.length-1; i++){
+            int x = a[i] + a[i+1];
+            ano[i] = x;
+        }
+        
+        sumTriangle(ano);
+
+        System.out.println(Arrays.toString(a));
+
+    }
+}
