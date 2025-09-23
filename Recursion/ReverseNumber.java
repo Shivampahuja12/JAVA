150e5d7ec636651fe666e366938d5ba90459670c
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-24 00:57:04 +0530

    Reverse Number add

---
+++ Recursion/ReverseNumber.java
@@ -0,0 +1,26 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        /* int ans = reverseNum(1505);
+        System.out.println(ans); */
+
+        int answer = reverseRecursion(145555, 0);
+        System.out.println(answer);
+
+    }
+    static int reverseRecursion(int n, int ans){
+        if (n==0) return ans;
+        ans = ans * 10 + (n%10); 
+        return reverseRecursion(n/10, ans);
+    }
+
+    static int reverseForLoop(int n){
+        int ans = 0;
+        int rem;
+
+        for (int i=n; i>0; i=i/10){
+            rem = i%10;
+            ans = ans * 10 + rem;
+        }
+        return ans;
+    }
+}
