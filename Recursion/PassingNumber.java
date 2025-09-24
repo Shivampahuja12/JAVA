19ab2887c6220c8baf84bdafe63ee6e05fce31b0
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-09-24 23:49:13 +0530

    Passing Number add

---
+++ Recursion/PassingNumber.java
@@ -0,0 +1,14 @@
+public class PassingNumber {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n){
+        if (n==0) return;
+
+        System.out.print(n + " ");
+        fun(--n);
+
+        // n-- vs --n
+    }
+    
+}
