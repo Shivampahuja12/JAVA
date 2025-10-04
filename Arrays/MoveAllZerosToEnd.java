215e2fd5ddec229ab87e3233ffc499284f22db03
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-10-04 22:02:15 +0530

    Optimized Solution add

--- Arrays/MoveAllZerosToEnd.java
+++ Arrays/MoveAllZerosToEnd.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 public class MoveAllZerosToEnd {
     public static void main(String[] args) {
         int[] a = {1, 0 ,2 ,3 ,0 ,4 ,0 ,1};
+        // int[] a = {0};
         
-        move1(a);
+        // move1(a);
+        // move2(a);
+        move3(a);
         System.out.println(Arrays.toString(a));
     }
 
@@ -22,4 +25,42 @@ public class MoveAllZerosToEnd {
         }
     }
 
+    static void move2(int[] a){
+        // optimized - two pointer/ in-place overwrite approach
+        //! not a proper two pointer approach
+
+        int idx = 0;
+        for (int i=0; i<a.length; i++){
+            if (a[i] != 0){
+                a[idx++] = a[i];
+            }
+        }
+        for (int i=idx; i<a.length; i++){
+            a[i] = 0;
+        }
+    }
+
+    static void move3(int[] a){
+        // * exact two pointer approach
+        // int i;
+        int j=-1;
+
+        for (int i=0; i<a.length; i++){
+            if (a[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if (j==-1) return;
+
+        for (int i=j+1; i<a.length; i++){
+            if (a[i] != 0){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                j++;
+            }
+        }
+    }
 }
