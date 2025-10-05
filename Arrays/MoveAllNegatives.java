9c17fce54e8f4050096e6193348a1791ba9abf41
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-10-05 18:43:27 +0530

    Move all negatives add

--- Arrays/MoveAllNegatives.java
+++ Arrays/MoveAllNegatives.java
@@ -3,10 +3,11 @@ import java.util.Arrays;
 
 public class MoveAllNegatives {
     public static void main(String[] args) {
-        int[] a = {1, -2, 3, -4, -5};   // → [-2, -4, -5, 1, 3]
+        // int[] a = {1, -2, 3, -4, -5};   // → [-2, -4, -5, 1, 3]
+        int[] a = {1, -2, 3, -4, -5, 2, 10, -12, 7};    // → [-2, -4, -5, -12, 1, 3, 2, 10, 7]
 
-        move1(a);
-
+        // move1(a);
+        move3(a);
         System.out.println(Arrays.toString(a));
 
 
@@ -15,7 +16,7 @@ public class MoveAllNegatives {
     static void move1(int[] a){
 
         // brute using extra space (least optimized)
-        
+
         ArrayList<Integer> list = new ArrayList<>();
 
         for (int i=0; i<a.length; i++){
@@ -36,6 +37,50 @@ public class MoveAllNegatives {
         for (int i=idx; i<a.length; i++){
             a[i] = list.get(k);
             k++;
+        }
+    }
+
+    static void move2(int[] a){
+        
+        // if order doesn't matter 
+
+        int j = -1;
+        for (int i=0; i<a.length; i++){
+            if (a[i] > 0){
+                j = i;
+                break;
+            }
+        }
+        if (j==-1) return;
+
+        for (int i=j+1; i<a.length; i++){
+            if(a[i] < 0){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    static void move3(int[] a){
+
+        // in this order matters - works like insertion sort...
+        
+        for (int i=0; i<a.length; i++){
+            
+            if (a[i] < 0){
+                int k = i-1;
+                int ele = a[i];
+                
+                while (k >= 0 && a[k] >= 0){
+                    a[k+1] = a[k];
+                    k--;
+                }
+                a[k+1] = ele;
+            }
+
+
         }
     }
 }
