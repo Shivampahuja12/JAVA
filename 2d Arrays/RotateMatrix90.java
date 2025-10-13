43a2d702a61aafae89502e46ef37366c5b170bff
Shivampahuja12 <shivampahuja1212@gmail.com>
2025-10-13 10:07:00 +0530

    rotate matrix by 90 deg MODIFY

--- 2d Arrays/RotateMatrix90.java
+++ 2d Arrays/RotateMatrix90.java
@@ -13,16 +13,42 @@ public class RotateMatrix90 {
         a = main;
         System.out.println(Arrays.deepToString(a));
 
+        int[][] b = { {1,2,3}, {4,5,6}, {7,8,9}};
+        rotateBy90(b);
+        System.out.println(Arrays.deepToString(b));
+
+    }
+
+    static void rotateBy90(int[][] mat){
+
+        // ! OPTIMAL- EFFICIENT CODE (IN-PLACE TRANSPOSE AND REVERSE)
+        for (int i=0; i<mat.length; i++){
+            for (int j=i+1; j<mat[0].length; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        for (int i=0; i<mat.length; i++){
+            for (int j=0; j<mat[0].length/2; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][mat.length-1-j];
+                mat[i][mat.length-1-j] = temp;
+            }
+        }
+
     }
 
     static int[][] transpose(int[][] mat) {
+
+        // ! BRUTE - NOT EFFICIENT (TAKING EXTRA SPACE)
         int[][] res = new int[mat.length][mat[0].length];
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
                 res[j][i] = mat[i][j];
             }
         }
-
         return res;
     }
 
@@ -34,7 +60,6 @@ public class RotateMatrix90 {
                 res[i][j] = mat[i][c++];
             }
         }
-
         return res;
     }
 
